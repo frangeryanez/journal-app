6a4bdc54954c06a7f954a62781c326e9c489d750
Franger Yanez <frangeryanez@gmail.com>
2022-09-26 14:19:59 -0400

    Add basic routes, define style, create theme, define layout

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,8 +1,10 @@
+import { AppRouter } from './router/AppRouter';
+import { AppTheme } from './theme';
+
 export const JournalApp = () => {
   return (
-    <>
-      <h1>Journal App</h1>
-      <hr />
-    </>
+    <AppTheme>
+      <AppRouter />
+    </AppTheme>
   )
 }
