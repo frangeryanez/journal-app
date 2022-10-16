10cf1413af93eaa2d7fd6a15ae6a9a4c66413741
Franger Yanez <frangeryanez@gmail.com>
2022-10-16 14:45:47 -0400

    Add empty note

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+import { startNewNote } from '../../store';
 import { AddOutlined } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
-import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+  const { isSaving, active } = useSelector(state => state.journal);
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  }
+
   return (
     <JournalLayout>
-      <NothingSelectedView />
+      {
+        (!!active)
+          ? <NoteView />
+          : <NothingSelectedView />
+      }
+
       <IconButton
+        disabled={ isSaving }
+        onClick={ onClickNewNote }
         size="large"
         sx={{
           color: "white",
